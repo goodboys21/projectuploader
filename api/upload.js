2f71190bbd3d7c16eaa74dc623f741d79bf3fab6
goodboys21 <akunceeingood5@gmail.com>
2025-02-08 14:41:18 +0700

    upload.js

--- api/upload.js
+++ api/upload.js
@@ -10,16 +10,23 @@ export default async function handler(req, res) {
 
         const uploadResponse = await fetch("https://cdn.acaw.my.id/upload", {
             method: "POST",
+            headers: {
+                "accept": "application/json"
+            },
             body: formData
         });
+
+        if (!uploadResponse.ok) {
+            return res.status(500).json({ message: "Gagal upload ke CDN Acaw" });
+        }
 
         const uploadData = await uploadResponse.json();
 
         if (!uploadData.fileDetails || !uploadData.fileDetails.fileName) {
-            return res.status(500).json({ message: "Gagal upload ke CDN Acaw" });
+            return res.status(500).json({ message: "Upload gagal, cek server!" });
         }
 
-        // Modifikasi URL agar menggunakan domain Vercel
+        // Ganti URL file dengan proxy domain lu
         const fileName = uploadData.fileDetails.fileName;
         const proxyUrl = `https://baguscdn.vercel.app/file/${fileName}`;
 
@@ -32,4 +39,4 @@ export default async function handler(req, res) {
     } catch (error) {
         res.status(500).json({ message: "Terjadi kesalahan pada server", error: error.message });
     }
-}
+    }
